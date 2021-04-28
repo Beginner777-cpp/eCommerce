5e2ff9d6534d18fbbfcaa2705d191110dbbd59fb
test <test>
2021-04-28 23:01:59 +0500

    updating project with collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Homepage />
+        <Route exact path='/' component={Homepage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './menu-item.style.scss';
-const MenuItem = ({title,imageUrl,size}) => (
-  <div  className={`${size} menu-item`}>
-    <div className="background-image" style={{
-    backgroundImage: `url(${imageUrl})`
-  }}>
-      
-    </div>
-    <div className='content'>
-      <div className='title'>{title.toUpperCase()}</div>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-);
+import { withRouter } from 'react-router-dom';
+const MenuItem = ({ title, imageUrl, size, history, linkUrl,match }) => {
+  return (
+    <div className={`${size} menu-item`} onClick={
+      () => history.push(`${match.url}${linkUrl}`)
+    }>
+      <div className="background-image" style={{
+        backgroundImage: `url(${imageUrl})`
+      }}>
 
-export default MenuItem;
+      </div>
+      <div className='content'>
+        <div className='title'>{title.toUpperCase()}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
+
+export default withRouter(MenuItem);
