8783f8070ded8d85b363c4aeac92eef92d9228ca
test <test>
2021-04-28 23:38:30 +0500

    added header component and some links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={Homepage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+        </Switch>
+
       </div>
     );
   }
